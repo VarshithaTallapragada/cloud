efd5f92a07b34161d2d237e39ac7e42ca71cbc46
T.Varshitha <23P31A0512@acet.ac.in>
2025-12-27 12:01:23 +0530

    Update ones.py

--- ones.py
+++ ones.py
@@ -1,1 +1,9 @@
-print('varsha')
+num = float(input("Enter a number: "))
+
+if num > 0:
+    print("The number is positive.")
+elif num == 0:
+    print("The number is zero.")
+else:
+    print("The number is negative.")
+
